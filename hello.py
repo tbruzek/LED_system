b5c6c46f67bc273b57f0c6c80f4aa00a6fc385f6
tbruzek <tbruzek@vt.edu>
2017-06-24 16:33:48 -0400

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def home():
+	return render_template('index.html')
+
+@app.route('/devices/0/turn_on', methods = ['POST'])
+def turn_on():
+	f = open('/tmp/light', 'w')
+	f.write('ON')
+	return render_template('index_1.html')
+
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0')
