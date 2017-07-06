19a6bce07926b366b15c71a458b0ecfdbe5fa11b
Tom Bruzek <tbruzek@vt.edu>
2017-07-05 20:30:27 -0400

    ..

--- hello.py
+++ hello.py
@@ -10,8 +10,7 @@ def home():
 @app.route('/devices/0/turn_on', methods = ['POST'])
 def turn_on():
     state = request.values.get('Data')
-    #return render_template('index_1.html', state = state)
-    return state
+    return render_template('index_1.html', state = state)
 
 #@app.route('/device_states', methods = ['POST'])
 #def change():
